a682a44b0110cca9a4f56160992d0dbc6a9ef418
Anton Lundin <anton.lundin@umu.se>
2020-01-21 11:42:16 +0100

    Automatic migration from freestyleJob to pipelineJob
    The downside with migrating from freestyleJob to pipelineJob is that we
    need to remove all the current freestylejobs from jenkins before job-dsl
    can create new pipelinejobs. job-dsl can't just change type on those old
    jobs, so this needs manual handing, and this will cause us to loose the
    old build history.

--- github_docker_repos.groovy
+++ github_docker_repos.groovy
@@ -2,6 +2,8 @@
 import groovy.json.JsonSlurper
 import java.io.IOException
 
+// TODO: Remove this when migration to pipelineJob is done
+import jenkins.model.Jenkins
 
 def orgs = ['SUNET','TheIdentitySelector']
 def api = "https://api.github.com"
@@ -72,6 +74,13 @@ for (org in orgs) {
                 if (repo.archived) {
                     out.println("Skipping archived repo")
                     continue
+                }
+
+                // TODO: Remove this when migration to pipelineJob is done
+                def existing_job = Jenkins.instance.getItem(repo.name)
+                if (existing_job && existing_job instanceof hudson.model.FreeStyleProject) {
+                    println("Deleting existing freestyleJob ${repo.name} to make room for pipelineJob")
+                    existing_job.delete()
                 }
 
                 pipelineJob(repo.name) {
